6c2373031e5600054848f90239bbc8a95c2172a4
fabianrguez <fabian1041993@gmail.com>
2021-12-20 12:09:45 +0000

    challenge 19 solved

--- src/challenge-19/challenge-19.js
+++ src/challenge-19/challenge-19.js
@@ -1,3 +1,17 @@
 export default function learn(time, courses) {
-   return [0, 0]
- }
+  let coursesIndex = [0, 0];
+  let maxAmountOfTime = 0;
+
+  for (let index = 0; index < courses.length; index++) {
+    for (let k = 0; k < courses.length; k++) {
+      if (index !== k) {
+        const amountOfTime = courses[index] + courses[k];
+        if (amountOfTime > maxAmountOfTime && amountOfTime <= time) {
+          maxAmountOfTime = amountOfTime;
+          coursesIndex = [index, k];
+        }
+      }
+    }
+  }
+  return maxAmountOfTime !== 0 ? coursesIndex : null;
+}
--- src/challenge-19/challenge-19.test.js
+++ src/challenge-19/challenge-19.test.js
@@ -5,4 +5,34 @@ describe('[challenge-19] - What should be learn in Platzi?', () => {
     const chapters = learn(10, [2, 3, 8, 1, 4]);
     expect(chapters).toStrictEqual([0, 2]);
   });
+
+  test('with 15 hours available, courses [2, 10, 4, 1]', () => {
+    const chapters = learn(15, [2, 10, 4, 1]);
+    expect(chapters).toStrictEqual([1, 2]);
+  });
+
+  test('with 25 hours available, courses [10, 15, 20, 5]', () => {
+    const chapters = learn(25, [10, 15, 20, 5]);
+    expect(chapters).toStrictEqual([0, 1]);
+  });
+
+  test('with 8 hours available, courses [8, 2, 1]', () => {
+    const chapters = learn(8, [8, 2, 1]);
+    expect(chapters).toStrictEqual([1, 2]);
+  });
+
+  test('with 8 hours available, courses [8, 2, 1, 4, 3]', () => {
+    const chapters = learn(8, [8, 2, 1, 4, 3]);
+    expect(chapters).toStrictEqual([3, 4]);
+  });
+
+  test('with 4 hours available, courses [10, 14, 20]', () => {
+    const chapters = learn(4, [10, 14, 20]);
+    expect(chapters).toBeNull();
+  });
+
+  test('with 5 hours available, courses [5, 5, 5]', () => {
+    const chapters = learn(5, [5, 5, 5]);
+    expect(chapters).toBeNull();
+  });
 });
